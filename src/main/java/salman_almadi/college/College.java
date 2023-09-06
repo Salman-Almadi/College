eede380dc19ae288768ef0aac464cefeb5084060
Salman Almadi <143233304+Salman-Almadi@users.noreply.github.com>
2023-09-06 22:03:49 +0300

    First commit!

---
+++ src/main/java/salman_almadi/college/College.java
@@ -0,0 +1,13 @@
+
+package salman_almadi.college;
+
+/**
+ *
+ * @author Salman Almadi
+ */
+public class College {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
